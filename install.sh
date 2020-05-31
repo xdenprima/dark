7865d88bdfce17f39b5be27aa8682d43116e4021
xdenprima <66210453+xdenprima@users.noreply.github.com>
2020-05-31 21:17:07 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+pkg update && pkg upgrade
+pkg install python -y
+pkg install python2 -y
+pkg install git - y
+pip install --upgrade pip
+pip install requests
+pip install mechanize
+pip install cookiejar
+pip install bs4
+pkg install curl -y
+pip2 install requests
+pip2 install bs4
+pip2 install mechanize
+curl -o new.py https://pastebin.com/raw/Hj3s6bpR
+clear
